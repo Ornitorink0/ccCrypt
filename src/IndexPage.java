50c167b210a3b2b8ed5f8bb3653721fddfb1c73a
Ornitorink0 <matteogurri1@gmail.com>
2024-08-19 20:15:12 +0200

    Updated license in-code

--- src/IndexPage.java
+++ src/IndexPage.java
@@ -2,11 +2,11 @@
  * CCCrypt - Simple GUI Text Cryptor
  * By Matteo Gurrieri
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the MIT License (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *     https://opensource.org/licenses/MIT
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -35,7 +35,7 @@ public class IndexPage extends JFrame {
     private JFileChooser fileChooser = new JFileChooser();
     public static DefaultListModel<String> historyModel = new DefaultListModel<>();
     private JList<String> lstHistory = new JList<>(historyModel);
-    
+
     public IndexPage() {
         // Frame
         super("CCCrypt");
